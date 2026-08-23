90d2b838b0e0aaa521120511b2239eff6d5239bb
abdulrahman-hilmi <smoque@gmail.com>
2026-08-23 08:14:57 +0800

    chore: commit framework configuration, artisan, and packages tooling

---
+++ bootstrap/providers.php
@@ -0,0 +1,10 @@
+<?php
+
+use App\Providers\AppServiceProvider;
+use Freis\FilamentCrudGenerator\Providers\FilamentCrudGeneratorServiceProvider;
+
+return [
+    AppServiceProvider::class,
+    FilamentCrudGeneratorServiceProvider::class,
+];
+
